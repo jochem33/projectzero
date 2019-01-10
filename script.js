21ae0a819478705ec048a4b99db3035e5fd4a45e
Luuk <luuknoverraz@gmail.com>
2019-01-10 15:33:39 +0100

    [change] Centered div horizontally and changed style
    Gotta center vertically now and add a few other things!

---
+++ script.js
@@ -0,0 +1,8 @@
+var tekst1 = document.getElementById("span1");
+tekst1.innerHTML = tekst1.innerHTML.fontcolor("blue");
+
+var tekst2 = document.getElementById("span2");
+tekst2.style.fontSize = "25px";
+
+var tekst3 = document.getElementById("span3");
+tekst3.style.font = "italic bold 20px arial,serif";
