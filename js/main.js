7f826b4f269ac7713fbb1fef9e6d8f88558f3b65
Jochem Baas <jochem@jochembaas.nl>
2019-01-12 18:44:40 +0100

    [change] adding new must-see's now works and it works more efficient
    It's now done in 5 lines instead of 18!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 function addMustsee() {
     mustSeeItems = document.getElementById("must-see-items");
-    mustSeeInputs = document.getElementsByClassName("overlay-must-see-input");
-    mustSeeValues = [];
-
-    newValues = [];
-    newValue = "";
-
-    for(item in mustSeeInputs) {
-        mustSeeValues.push(item.value);
-    }
-    for(item in mustSeeValues) {
-        newValues.push('<input class="overlay-must-see-input" type="text" placeholder="tap to type location" value="' + item + '"></input>');
-    }
-    newValues.push('<input class="overlay-must-see-input" type="text" placeholder="tap to type location"></input>');
-
-    for(item in newValues) {
-        newValue = newValue + item;
-    }
-
-    mustSeeItems = newValue;
+    var input = document.createElement("input");
+    input.type = "text";
+    input.className = "overlay-must-see-input";
+    input.placeholder = "tap to type location"
+    mustSeeItems.appendChild(input);
 }
