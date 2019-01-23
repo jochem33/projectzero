d57d0d94f691bea06ec6efbbd2f392385916b87a
Jochem Baas <jochem@jochembaas.nl>
2019-01-23 17:17:11 +0100

    [remove] sdk files so merge is piossible

--- js/map.js
+++ js/map.js
@@ -21,3 +21,22 @@ function searchFromHeader() {
         map.setView([lat, lon], 12);
     }
 }
+
+
+function placeToCoordinates(searchQuery) {
+    const http = new XMLHttpRequest();
+    const url='https://api.tomtom.com/search/2/search/' + searchQuery + '.JSON?key=G0IxuAKMgahregQDlzKW1bBbKkyWQGAt&typeahead=false&limit=10';
+    http.open("GET", url);
+    http.setRequestHeader("Content-Type", "text/plain");
+    http.send();
+    http.onreadystatechange=(e)=>{
+        resultsResponse = JSON.parse(http.responseText);
+
+        var lat = resultsResponse["results"][0]["position"]["lat"];
+        var lon = resultsResponse["results"][0]["position"]["lon"];
+        console.log([lat, lon])
+
+
+        //code to run
+    }
+}
