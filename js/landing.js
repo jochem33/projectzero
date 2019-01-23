80548c93644464175c7fbe6cd4f5fc73a2c5ed63
CoderClassLuuk <luuk.noverraz@coderclass.nl>
2019-01-23 16:33:23 +0100

    [change] cleaned up code and better mobile support
    tons of unneccesary js deleted and added a small scale change too

--- js/landing.js
+++ js/landing.js
@@ -1,40 +1,7 @@
-var Environment = {
-    //mobile or desktop compatible event name, to be used with '.on' function
-    TOUCH_DOWN_EVENT_NAME: 'mousedown touchstart',
-    TOUCH_UP_EVENT_NAME: 'mouseup touchend',
-    TOUCH_MOVE_EVENT_NAME: 'mousemove touchmove',
-    TOUCH_DOUBLE_TAB_EVENT_NAME: 'dblclick dbltap',
-
-    isAndroid: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    isBlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    isIOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    isOpera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    isWindows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    isMobile: function() {
-        return (Environment.isAndroid() || Environment.isBlackBerry() || Environment.isIOS() || Environment.isOpera() || Environment.isWindows());
-    }
-};
-
 function hover(element) {
   element.setAttribute("src", "images/scroll_arrow_hover.png");
 }
 
 function unhover(element) {
   element.setAttribute("src", "images/scroll_arrow.png");
-}
-
-if (Environment.isMobile() == true) {
-  var landingbody = document.getElementById("landingbody");
-  console.log(landingbody);
-  landingbody.style.background = "url(images/bg.png) repeat 50% 50%";
 }
