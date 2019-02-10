b556aa6d363d7ebf673eb6551b42b871c8e5e597
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-02-10 10:24:44 +0100

    [change] map.js and map2.js combined and olf map.js put back for merging

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
-//api key G0IxuAKMgahregQDlzKW1bBbKkyWQGAt
+// //api key G0IxuAKMgahregQDlzKW1bBbKkyWQGAt
+// firsttime = true;
 
-//init map
 var map = tomtom.L.map('map', { 
     key: 'G0IxuAKMgahregQDlzKW1bBbKkyWQGAt', 
     basePath: 'sdk', 
@@ -8,9 +8,47 @@ var map = tomtom.L.map('map', {
     zoom: 9
 });
 
+var markerOptionsFinish = {
+    icon: tomtom.L.icon({
+        iconUrl: 'images/finish.png',
+        iconSize: [30, 34],
+        iconAnchor: [15, 34]
+    })
+};
 
-var aanvullendelocaties = [];
-var routelocaties = [];
+var markerOptionsWaypiont = {
+    icon: tomtom.L.icon({
+        iconUrl: 'images/waypoint.png',
+        iconSize: [30, 34],
+        iconAnchor: [15, 34]
+    })
+};
+
+var markerOptionsStart = {
+    icon: tomtom.L.icon({
+        iconUrl: 'images/start.png',
+        iconSize: [30, 34],
+        iconAnchor: [15, 34]
+    })
+};
+
+
+function searchFromHeader() {
+    searchBlock = document.getElementById("search-block");
+
+    const http = new XMLHttpRequest();
+    const url='https://api.tomtom.com/search/2/search/' + searchBlock.value + '.JSON?key=G0IxuAKMgahregQDlzKW1bBbKkyWQGAt&typeahead=false&limit=10';
+    http.open("GET", url);
+    http.setRequestHeader("Content-Type", "text/plain");
+    http.send();
+    http.onreadystatechange=(e)=>{
+        resultsResponse = JSON.parse(http.responseText);
+
+        var lat = resultsResponse["results"][0]["position"]["lat"];
+        var lon = resultsResponse["results"][0]["position"]["lon"];
+        map.setView([lat, lon], 12);
+    }
+}
 
 
 var offset = 4;
@@ -18,40 +56,96 @@ var formData = document.getElementById("overlay-form");
 var place = []
 var rootlocations;
 
+function generateRoute(form){
 
-async function generateRoute(form){
-    start = await textToLatLonRequest(form.elements[0].value)
-    routelocaties.push(start[0] + "," + start[1])
 
-    await extralocaties(form);
+    addToPlaces(form, 0, false);
 
-    for(var i = offset; i < numberOffMustSees + offset; i++){        
-        var locatie = await textToLatLonRequest(form.elements[i].value);
-        routelocaties.push(locatie[0] + "," + locatie[1]);
+    for(var i = offset; i < numberOffMustSees + offset; i++){
+        // if (i == numberOffMustSees + offset - 1){
+        //     lastTime = true;
+        // } else{
+        //     lastTime = false;
+        // }
+        console.log(i);
+        addToPlaces(form, i, false);
     }
 
-    end = await textToLatLonRequest(form.elements[1].value)
-    routelocaties.push(end[0] + "," + end[1])
+    addToPlaces(form, 1, false);
+
+    setTimeout(function(){
+        routes()
+    }, 1000);
+
+    
+};
 
 
+function routes() {
+    rootlocations = "";
+    for (var i = 0; i < place.length; i++){
+        console.log(i)
+        var plc = place[i].split(",")
 
-    var rootlocations = "";
-    for (var i = 0; i < routelocaties.length; i++){
-        var currentLatLon = routelocaties[i].split(",");
-        var marker = tomtom.L.marker([currentLatLon[0], currentLatLon[1]]).addTo(map);
-        rootlocations = (rootlocations + ":" + (routelocaties[i]).toString())
+        if (i == place.length - 1){
+            var marker = tomtom.L.marker(plc, markerOptionsFinish).addTo(map);
+        }   
+        else if (i == 0){
+            var marker = tomtom.L.marker(plc, markerOptionsStart).addTo(map);
+        }   
+        else if (i != 0){
+            var marker = tomtom.L.marker(plc, markerOptionsWaypiont).addTo(map);
+        }
+
+        rootlocations = (rootlocations + ":" + (place[i]).toString())
+
+
     }
+    // for (var i = 0; i < place.length; i++){
+    //     if(i=0){
+    //         rootlocations = place[i].toString()
+    //     } else {
+    //         rootlocations = (rootlocations + ":" + (place[i]).toString())
+    //     }
+    // }
 
     rootlocations = rootlocations.substr(1);
     
-    await tomtom.routing() 
+    tomtom.routing() 
         .locations(rootlocations)
         .go().then(function(routeJson) {
             var route = tomtom.L.geoJson(routeJson, {
                 style: {color: '#8800EB', opacity: 0.6, weight: 4}
             }).addTo(map);
-            map.fitBounds(route.getBounds(), {padding: [5, 5]});
+            map.fitBounds(route.getBounds(), {padding: [10, 10]});
     });
+}
 
-    hideGenerateOverlay()
-};
+
+
+function addToPlaces(form, formIndex, lastTime) {
+    console.log("doing addtoplaces");
+    firsttime = true;
+    const http = new XMLHttpRequest();
+    const url='https://api.tomtom.com/search/2/search/' + form.elements[formIndex].value+ '.JSON?key=G0IxuAKMgahregQDlzKW1bBbKkyWQGAt&typeahead=false&limit=10';
+    http.open("GET", url);
+    http.setRequestHeader("Content-Type", "text/plain");
+    http.send();
+    http.onreadystatechange=(e)=>{
+        resultsResponse = JSON.parse(http.responseText);
+
+
+        var lat = resultsResponse["results"][0]["position"]["lat"];
+        var lon = resultsResponse["results"][0]["position"]["lon"];
+
+        place.push(lat + "," + lon);
+
+        
+        console.log(lastTime)
+        if (lastTime) {
+            
+            console.log("routesss");
+            routes();
+        }
+    }
+}
--- js/map2.js
+++ js/map2.js
@@ -1,5 +1,61 @@
 //api key G0IxuAKMgahregQDlzKW1bBbKkyWQGAt
 var time;
+var aanvullendelocaties = [];
+var routelocaties = [];
+
+
+var offset = 4;
+var formData = document.getElementById("overlay-form");
+var place = []
+var rootlocations;
+
+//api key G0IxuAKMgahregQDlzKW1bBbKkyWQGAt
+
+//init map
+var map = tomtom.L.map('map', { 
+    key: 'G0IxuAKMgahregQDlzKW1bBbKkyWQGAt', 
+    basePath: 'sdk', 
+    center: [35.667998, 139.783823], 
+    zoom: 9
+});
+
+
+async function generateRoute(form){
+    start = await textToLatLonRequest(form.elements[0].value)
+    routelocaties.push(start[0] + "," + start[1])
+
+    await extralocaties(form);
+
+    for(var i = offset; i < numberOffMustSees + offset; i++){        
+        var locatie = await textToLatLonRequest(form.elements[i].value);
+        routelocaties.push(locatie[0] + "," + locatie[1]);
+    }
+
+    end = await textToLatLonRequest(form.elements[1].value)
+    routelocaties.push(end[0] + "," + end[1])
+
+
+
+    var rootlocations = "";
+    for (var i = 0; i < routelocaties.length; i++){
+        var currentLatLon = routelocaties[i].split(",");
+        var marker = tomtom.L.marker([currentLatLon[0], currentLatLon[1]]).addTo(map);
+        rootlocations = (rootlocations + ":" + (routelocaties[i]).toString())
+    }
+
+    rootlocations = rootlocations.substr(1);
+    
+    await tomtom.routing() 
+        .locations(rootlocations)
+        .go().then(function(routeJson) {
+            var route = tomtom.L.geoJson(routeJson, {
+                style: {color: '#8800EB', opacity: 0.6, weight: 4}
+            }).addTo(map);
+            map.fitBounds(route.getBounds(), {padding: [5, 5]});
+    });
+
+    hideGenerateOverlay()
+};
 
 
 async function extralocaties(form) {
